328a41f1ebd8482a7434067fcc834feac087ba89
Keishin CHOU <keishin.chou@gmail.com>
2019-11-24 23:01:41 +0900

    Created stack view.

--- Multitasking/ViewController.swift
+++ Multitasking/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setDefaultTitle()
+        
+        let add = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWebView))
+        let delete = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteWebView))
+        navigationItem.rightBarButtonItems = [delete, add]
     }
+    
+    @objc func addWebView() {
+        let webView = WKWebView()
+        webView.navigationDelegate = self
+        
+        stackView.addArrangedSubview(webView)
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+    }
+    
+    @objc func deleteWebView() {
+        
+    }
+    
+    func setDefaultTitle() {
+        title = "Multibrowser"
+    }
+    
+    
 
 
 }
-
